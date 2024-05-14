50cdcbeaa31b12c9c18b9a77aa938336d6b8bed4
vanduc2514 <vanduc2514@gmail.com>
2024-05-14 18:20:25 +0000

    add debug endpoint for controller

--- src/main/java/tech/demo/springai/apis/CodeAssistantController.java
+++ src/main/java/tech/demo/springai/apis/CodeAssistantController.java
@@ -4,34 +4,40 @@ import org.springframework.ai.chat.ChatClient;
 import org.springframework.ai.chat.ChatResponse;
 import org.springframework.ai.chat.StreamingChatClient;
 import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import reactor.core.publisher.Flux;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class CodeAssistantController {
 
+    @Autowired
     private ChatClient chatClient;
 
+    @Autowired
     private StreamingChatClient streamingChatClient;
 
-    public CodeAssistantController(ChatClient chatClient, StreamingChatClient streamingChatClient) {
-        this.chatClient = chatClient;
-        this.streamingChatClient = streamingChatClient;
+    @GetMapping("/debug")
+    public ChatResponse debug(@RequestParam String prompt) {
+        return chatClient.call(new Prompt(prompt));
+    }
+
+    @GetMapping("/debug/stream")
+    public Flux<ChatResponse> debugStream(@RequestParam String prompt) {
+        return streamingChatClient.stream(new Prompt(prompt));
     }
 
     @GetMapping("/code/review")
     public ChatResponse codeReview(String codeSnippet) {
-        // TODO: use template for prompting
-        Prompt codeReviewPrompt = new Prompt("Tell me a random software engineer joke");
-        return chatClient.call(codeReviewPrompt);
+        throw new UnsupportedOperationException("Not Yet Implemneted");
     }
 
     @GetMapping("/code/review/stream")
     public Flux<ChatResponse> codeReviewStreaming(String codeSnippet) {
-        // TODO: use template for prompting
-        Prompt codeReviewPrompt = new Prompt("Tell me a random software engineer joke");
-        return streamingChatClient.stream(codeReviewPrompt);
+        throw new UnsupportedOperationException("Not Yet Implemneted");
     }
 }
