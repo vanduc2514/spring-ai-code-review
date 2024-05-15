f92b85ad83995bd912be342fe58e92c99523197f
vanduc2514 <vanduc2514@gmail.com>
2024-05-15 17:23:55 +0000

    adjust logic assessment prompt

--- src/main/java/tech/demo/springai/service/LogicAssementAgent.java
+++ src/main/java/tech/demo/springai/service/LogicAssementAgent.java
@@ -10,18 +10,17 @@ import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.chat.prompt.SystemPromptTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
-
-import com.google.api.client.util.Value;
 
 @Service
 public class LogicAssementAgent implements AssessmentAgent {
 
-    @Value("${classpath:/prompts/system-logic-assesment-message.st}")
+    @Value("classpath:prompts/system-logic-assesment-message.st")
     private Resource systemPromptResource;
 
-    @Value("${classhpath:/prompts/user-code-review-message.st}")
+    @Value("classpath:/prompts/user-code-review-message.st")
     private Resource userPromptResource;
 
     @Autowired
@@ -30,7 +29,7 @@ public class LogicAssementAgent implements AssessmentAgent {
     @Override
     public ChatResponse assessCodeSnippet(String codeSnippet) {
         Message systemMessage = new SystemPromptTemplate(systemPromptResource)
-            .createMessage();
+                .createMessage();
         Message userMessage = new PromptTemplate(userPromptResource)
                 .createMessage(Map.of("codeSnippet", codeSnippet));
         Prompt prompt = new Prompt(List.of(systemMessage, userMessage));
