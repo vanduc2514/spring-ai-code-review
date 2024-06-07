3d2ec035424d6f2ca80a88b50b51e2540048c462
hungdangvan <37893851+hungdangvan@users.noreply.github.com>
2024-06-07 14:59:14 +0700

    Security assessment agent (#2)
    Add implementation for Performance and Security Assessment Agent
     
    Co-authored-by: vanduc2514 <vanduc2514@gmail.com>

--- src/main/java/tech/demo/springai/apis/CodeAssistantController.java
+++ src/main/java/tech/demo/springai/apis/CodeAssistantController.java
@@ -22,6 +22,8 @@ import tech.demo.springai.dtos.RefactorCodeResponse;
 import tech.demo.springai.service.LogicAssementAgent;
 import tech.demo.springai.service.QualityAssessmentAgent;
 import tech.demo.springai.service.RefactorAgent;
+import tech.demo.springai.service.PerformanceAssessmentAgent;
+import tech.demo.springai.service.SecurityAssessmentAgent;
 
 
 @RestController
@@ -34,21 +36,29 @@ public class CodeAssistantController {
     private QualityAssessmentAgent qualityAssessmentAgent;
 
     @Autowired
+    private PerformanceAssessmentAgent performanceAssessmentAgent;
+
+    @Autowired
+    private SecurityAssessmentAgent securityAssessmentAgent;
+
+    @Autowired
     private RefactorAgent refactorAgent;
 
-    @PostMapping(
-        value = "/stream/code/review/stage/0",
-        consumes = MediaType.TEXT_PLAIN_VALUE,
-        produces = MediaType.APPLICATION_NDJSON_VALUE
-    )
-    public Flux<AgentResponse> streamAssessment(@RequestBody String codeSnippet) {
+    @PostMapping("/stream/code/review/stage/0")
+    public Flux<AgentResponse> streamCodeReviewStage0(@RequestBody String codeSnippet) {
         return Flux.merge(
                 logicAssementAgent.assessCodeSnippet(codeSnippet)
                         .map(chatResponse -> mapAgentResponse(
                                 chatResponse, AgentType.LOGIC_ASSESSMENT)),
                 qualityAssessmentAgent.assessCodeSnippet(codeSnippet)
                         .map(chatResponse -> mapAgentResponse(
-                                chatResponse, AgentType.QUALITY_ASSESSMENT)));
+                                chatResponse, AgentType.QUALITY_ASSESSMENT)),
+                performanceAssessmentAgent.assessCodeSnippet(codeSnippet)
+                        .map(chatResponse -> mapAgentResponse(
+                                chatResponse, AgentType.PERFORMANCE_ASSESSMENT)),
+                securityAssessmentAgent.assessCodeSnippet(codeSnippet)
+                        .map(chatResponse -> mapAgentResponse(
+                                chatResponse, AgentType.SECURITY_ASSESSMENT)));
     }
 
     @PostMapping(
